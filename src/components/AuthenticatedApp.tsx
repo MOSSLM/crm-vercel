031d32123e764cf1037a7864e20071af6d26c2c8
MOSSLM <codingmos@gmail.com>
2025-08-21 23:32:45 +0200

    fixed applayout import

--- src/components/AuthenticatedApp.tsx
+++ src/components/AuthenticatedApp.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { NavigationProvider } from './routing/NavigationContext';
-import { AppLayout } from './layout/AppLayout';
+import  AppLayout  from './layout/AppLayout';
 import { PageRouter } from './routing/PageRouter';
 
 export const AuthenticatedApp: React.FC = () => {
